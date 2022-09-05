6b73488a410e92755fc560d28f6eca49115bfff4
Nuttapon_Sartmanee <nuttapon.a6@mail.kmutt.ac.th>
2022-09-05 23:16:28 +0700

    nuttapon026: Refac NPCtalk 05/09/2022 23.16

--- Assets/Script/NPC/NPCtalk.cs
+++ Assets/Script/NPC/NPCtalk.cs
@@ -2,18 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
-
 public class NPCtalk : MonoBehaviour
 {
     public TMP_Text Talk;
-  
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -25,7 +16,9 @@ public class NPCtalk : MonoBehaviour
     {
         speak.SetText("Welcome to Night Village");
         Talk.gameObject.SetActive(true);
+
         yield return new WaitForSeconds(3);
+
         speak.gameObject.SetActive(false);
     }
 }
