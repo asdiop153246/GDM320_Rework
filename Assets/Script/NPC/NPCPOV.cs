3a159e0c14d488fcf1036bdd6bacc6c6b31c5317
Nuttapon_Sartmanee <nuttapon.a6@mail.kmutt.ac.th>
2022-09-05 23:12:32 +0700

    nuttapon026: Refac NPCPOV 05/09/2022 23.12

--- Assets/Script/NPC/NPCPOV.cs
+++ Assets/Script/NPC/NPCPOV.cs
@@ -11,36 +11,33 @@ public class NPCPOV : MonoBehaviour
     public GameObject Playercam, NPCcam;
     public GameObject Char;
 
-    private void Start()
-    {
-        
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            
-            Playercam.SetActive(false);
-            NPCcam.SetActive(true);
+            setCameraToNPC();
             StartCoroutine(Speech(Talk));
-            
         }
     }
     IEnumerator Speech(TMP_Text speak)
     {
-        
         speak.SetText("Jump over the waterfall");
         Talk.gameObject.SetActive(true);
+
         yield return new WaitForSeconds(3);
+
         speak.gameObject.SetActive(false);
+        setCameraToPlayer();
+    }
+    private void setCameraToNPC()
+    {
+        Playercam.SetActive(false);
+        NPCcam.SetActive(true);
+    }
+    private void setCameraToPlayer()
+    {
         NPCcam.SetActive(false);
         Playercam.SetActive(true);
-        
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
