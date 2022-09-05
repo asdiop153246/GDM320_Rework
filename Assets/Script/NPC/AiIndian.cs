022f3990a84e4d7c6f3f69807548442fdb7d2ce1
Nuttapon_Sartmanee <nuttapon.a6@mail.kmutt.ac.th>
2022-09-05 22:52:15 +0700

    nuttapon026: Refac AiIndian 05/09/2022 22.51

--- Assets/Script/NPC/AiIndian.cs
+++ Assets/Script/NPC/AiIndian.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
+using UnityEngine.InputSystem.Utilities;
 
 public class AiIndian : MonoBehaviour
 {
-    public float speed = 200f;
+    public float moveSpeed = 200f;
     public float nextWayPointDistance = 3f;
     public Transform target;
 
@@ -15,13 +16,18 @@ public class AiIndian : MonoBehaviour
     Seeker seeker;
     Rigidbody2D rb;
 
-    // Start is called before the first frame update
     void Start()
+    {
+        aiGetComponent();
+
+        //seeker.StartPath(rb.position, target.position, OnPathComplete);
+        InvokeRepeating("UpdatePath", 1, 0.5f);
+    }
+
+    private void aiGetComponent()
     {
         seeker = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
-        //seeker.StartPath(rb.position, target.position, OnPathComplete);
-        InvokeRepeating("UpdatePath", 1, 0.5f);
     }
 
     void UpdatePath()
@@ -42,7 +48,22 @@ public class AiIndian : MonoBehaviour
 
     void Update()
     {
-        if (path == null) return;
+        checkWaypoint();
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWayPoint] - rb.position).normalized;
+        Vector2 force = direction * moveSpeed * Time.deltaTime;
+        rb.AddForce(force);
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWayPoint]);
+
+        checkNextWayPoint(distance);
+        checkFlip(force);
+    }
+    private void checkWaypoint()
+    {
+        if (path == null)
+        {
+            return;
+        }
         if (currentWayPoint >= path.vectorPath.Count)
         {
             reachedEndOfPath = true;
@@ -52,16 +73,18 @@ public class AiIndian : MonoBehaviour
         {
             reachedEndOfPath = false;
         }
+    }
 
-        Vector2 direction = ((Vector2)path.vectorPath[currentWayPoint] - rb.position).normalized;
-        Vector2 force = direction * speed * Time.deltaTime;
-        rb.AddForce(force);
-        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWayPoint]);
+
+    private void checkNextWayPoint(float distance)
+    {
         if (distance < nextWayPointDistance)
         {
             currentWayPoint++;
         }
-
+    }
+    private void checkFlip(Vector2 force)
+    {
         if (force.x >= 0.01f)
         {
             transform.localScale = new Vector3(1, 1, 1);
