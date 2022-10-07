0301627cce77ef7901c1d6601d12831c380d5b08
Nuttapon_Sartmanee <nuttapon.a6@mail.kmutt.ac.th>
2022-10-07 11:59:33 +0700

    nuttapon026: Add Method getInputChecker , getInputKeyboard

--- Assets/Script/Player/charscript.cs
+++ Assets/Script/Player/charscript.cs
@@ -19,7 +19,7 @@ public class charscript : MonoBehaviour
     AudioSource audioSource;
     SwapCamera camera;
 
-
+    [SerializeField] getInputChecker inputChecker;
 
     // Start is called before the first frame update
     void Start()
