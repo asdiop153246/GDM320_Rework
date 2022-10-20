9b9fc16fd7ebc7747105adb251633a617f2d6575
asdiop153246 <magicsiam001@gmail.com>
2022-10-20 23:33:54 +0700

    Update InputChecker

--- Assets/Script/Player/getInputChecker.cs
+++ Assets/Script/Player/getInputChecker.cs
@@ -2,7 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class getInputChecker : MonoBehaviour
+public static class getInputChecker
 {
+    public static bool IsJumpInput()
+    {
+        return Input.GetKeyDown(KeyCode.Space);
+    } 
 
+    public static bool IsPauseInput()
+    {
+        return Input.GetKeyDown(KeyCode.P);
+    }
+
+    public static bool IsAttackInput()
+    {
+        return Input.GetButtonDown("Fire1");
+    }
 }
